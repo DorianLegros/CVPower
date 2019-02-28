3e5e58c8cb0bb2b9f1a316dc8a2de03cca9375ea
BaptisteBobot <bobot.baptiste@gmail.com>
2019-02-28 11:27:26 +0100

    reset password

---
+++ application/views/user/resetpassword/resetpassword.php
@@ -0,0 +1,33 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<div class="container">
+    <div class="row">
+        <?php if (validation_errors()) : ?>
+            <div class="col-md-12">
+                <div class="alert alert-danger" role="alert">
+                    <?= validation_errors() ?>
+                </div>
+            </div>
+        <?php endif; ?>
+        <?php if (isset($error)) : ?>
+            <div class="col-md-12">
+                <div class="alert alert-danger" role="alert">
+                    <?= $error ?>
+                </div>
+            </div>
+        <?php endif; ?>
+        <div class="col-md-12">
+            <div class="page-header">
+                <h1>Login</h1>
+            </div>
+            <?= form_open() ?>
+            <div class="form-group">
+                <label for="mail">Adresse mail</label>
+                <input type="text" class="form-control" id="mail" name="mail" placeholder="Votre adresse mail">
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-default" value="Envoyer">
+            </div>
+            </form>
+        </div>
+    </div><!-- .row -->
+</div><!-- .container -->
