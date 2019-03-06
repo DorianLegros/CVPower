31b5bf70636cadf2ed02b4f2d921a6f282deb3a2
jonre <jon.renault@protonmail.com>
2019-03-06 13:10:48 +0100

    fusion front

--- application/views/templates/head.php
+++ application/views/templates/head.php
@@ -5,6 +5,7 @@
     <title><?php echo $title; ?></title>
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
     <link rel="stylesheet" href="<?php if(validation_errors()) { echo "../";} ?>css/style.css">
+    <link rel="icon" type="image/png" href="img/logo-blue.png" />
 </head>
 
 <body>
